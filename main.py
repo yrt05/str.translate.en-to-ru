96da0fe5fd63936009f994668985c1dfe3531746
fatx <62420897+yrt05@users.noreply.github.com>
2022-04-25 10:43:37 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import srt
+from googletrans import Translator
+from pathlib import Path
+import os
+
+translator = Translator()
+
+def transl(file):
+    with open(file,'r') as data:
+        subtitles = list(srt.parse(data))
+
+    for i in range(len(subtitles)):
+        q = subtitles[i].content
+        ru_subs = translator.translate(q, dest='ru', src='en')
+        subtitles[i].content = ru_subs.text
+
+    text = srt.compose(subtitles)
+    with open(file,'w') as file:
+        file.write(text)
+
+p = Path.cwd()
+
+for foldername, subfolders, filenames in os.walk(p):
+    for filename in filenames:
+        if filename[-4:] == '.srt':
+            pat = foldername + '/' + filename
+            p = Path(pat)
+            print(f'Переводится файл {p.name} из папки {p.parent}')
+            transl(pat)
+
