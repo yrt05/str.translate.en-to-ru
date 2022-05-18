0703a97e899b04866648e2a56580613c68f52888
yrt05 <62420897+yrt05@users.noreply.github.com>
2022-05-18 19:13:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
-import srt
-from googletrans import Translator
 from pathlib import Path
 import os
+import string
+import srt
+from googletrans import Translator
+
 
 translator = Translator()
 
-def transl(file):
+def filter_for_transl_files(j: str) -> bool:
+    return all([True if i not in string.ascii_letters else False for i in j])
+    
+    
+def transl(file) -> None:
+    '''file translate function'''
     with open(file,'r') as data:
         subtitles = list(srt.parse(data))
 
-    for i in range(len(subtitles)):
-        q = subtitles[i].content
-        ru_subs = translator.translate(q, dest='ru', src='en')
-        subtitles[i].content = ru_subs.text
+    first_word = subtitles[0].content.split()[0]
+    if not filter_for_transl_files(first_word):
+        for i in range(len(subtitles)):
+            q = subtitles[i].content
+            ru_subs = translator.translate(q, dest='ru', src='en')
+            subtitles[i].content = ru_subs.text
 
-    text = srt.compose(subtitles)
-    with open(file,'w') as file:
-        file.write(text)
-
-p = input("Введите путь к директорию с субтитрами:")
-
-for foldername, subfolders, filenames in os.walk(p):
-    for filename in filenames:
-        if filename[-4:] == '.srt':
-            p = os.path.join(foldername, filename)
-            print(f'Переводится файл {p.name}...')
-            transl(pat)
+        text = srt.compose(subtitles)
+        with open(file,'w') as file:
+            file.write(text)
             
-print("Перевод успешно завершен!")
+    else:
+        pass
+
+
+def main(p) -> None:
+    path = Path(p)
+    if p.endswith('.srt'):
+        print(f'Переводится файл {path.name}...')
+        transl(path)
+    else:
+        for foldername, subfolders, filenames in os.walk(p):
+            for filename in filenames:
+                if filename[-4:] == '.srt':
+                    path = os.path.join(foldername, filename)
+                    print(f'Переводится файл {path.name}...')
+                    transl(path)
+
+
+if __name__ == '__main__':
+    p = input("Введите путь к директорию с субтитрами:")
+    main(p)
+    print("Перевод успешно завершен!")
